23c40144b5d5076b62fdbf76b0eca79e7f353769
chanktb <4204179+chanktb@users.noreply.github.com>
2025-10-02 09:55:05 +0700

    Auto-update crawled data at 2025-10-04 23:08:17

--- utils/constants.py
+++ utils/constants.py
@@ -15,7 +15,7 @@ ENV_FILE = os.path.join(BASE_DIR, '.env')
 # --- Constants ---
 MAX_URLS = 500
 MAX_PREVNEXT_URLS = 100
-MAX_API_PAGES = 1
+MAX_API_PAGES = 2
 DEFAULT_API_URL_PATTERN = "https://{domain}/wp-json/wp/v2/product?per_page=100&page={page}&orderby=date&order=desc"
 HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"}
 REPO_URL_PATTERN = "https://raw.githubusercontent.com/ktbteam/productcrawler/main/domain/{domain}.txt"
